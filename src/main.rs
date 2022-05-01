5acb13c63524b010e737eced0fa523193d71a9ac
Marcel Maage <mmaage92@googlemail.com>
2022-05-01 10:25:25 +0200

    Updated copyright

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+/*********************************************************************
+ * Stm32f1_rust_can
+ *
+ * Copyright (C) 2022 Marcel Maage
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LICENSE file for more details.
+ */
+
 //! Simple CAN example.
 //! Requires a transceiver connected to PA11, PA12 (CAN1) or PB5 PB6 (CAN2).
 
